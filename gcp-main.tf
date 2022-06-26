2c5b693f864f8f239b6131e3806ec65e99ae28b9
JVennom <105567595+JVennom@users.noreply.github.com>
2022-06-26 00:50:23 -0300

    GCP

---
+++ gcp-main.tf
@@ -0,0 +1,13 @@
+resource "google_app_engine_application" "app" {
+  project     = "lab-devops-cloud-7asoo "
+  location_id = "us-west1"
+}
+
+resource "google_artifact_registry_repository" "my-repo" {
+  provider = google-beta
+
+  location = "us-west1"
+  repository_id = "labdevops"
+  description = "Imagens Docker"
+  format = "DOCKER"
+}
